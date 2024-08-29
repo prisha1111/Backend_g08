5d01104a9ca195e25600e6c9905882a05e540380
My Name <prisha682.be22@chitkara.edu.in>
2024-08-29 20:56:40 +0530

    mongodb-finding specific data using  operators

---
+++ 2908/README.md
@@ -0,0 +1,6 @@
+Tasks Completed
+Find the data where student has age = 22 and marks = 90
+
+Find the student whose marks is >= 80 and age >= 18
+
+Find whose grades are A++
---
+++ 2908/codee.js
@@ -0,0 +1,42 @@
+const students = [
+    {
+    name: "Trisha",
+    age: 20,
+    marks:45,
+    subject: "English"
+    },
+    {
+    name: "John",
+    age: 22,
+    marks: 50,
+    subject: "Math"
+    },
+    {
+      name: "Emily",
+      age: 21,
+      marks: 48,
+      subject: "Science"
+    },
+    {
+      name: "Michael",
+      age: 20,
+      marks: 42,
+      subject: "History" 
+    },
+    {
+      name: "Sarah",
+    age: 23,
+    marks: 56,
+    subject: "Computer Science"},
+    {
+      name: "David",
+      age: 24,
+      marks: 52,
+      subject: "Maths"
+      },
+    {name: "Ram",
+      age: 25,
+      marks: 58,
+      subject: "English"}
+    
+  ]
